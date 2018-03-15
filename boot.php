b810ca8f1d842c3a018b0aa59a5cc32ae5a5c01a
alexanderlexis <alexander.lexis@gmail.com>
2018-03-15 15:41:16 +0100

    werkt met geluid

--- boot.php
+++ boot.php
@@ -19,9 +19,9 @@ $cor = $_GET['cora'];
 
 
 if(in_array($cor, $allShips)){
-    echo '<div id="hit"><img src="imgs/boom.gif" id="boom"></div>';
+    echo '<div id="hit"><img src="imgs/boom.gif" id="boom"></div><audio autoplay><source src="sounds/boom.mp3"></audio>';
 }else {
-    echo '<div id="miss"></div>';
+    echo '<div id="miss"></div><audio autoplay><source src="sounds/splash.mp3"></audio>';
 }
 
 //if(in_array($cor, $submarine->cord)){

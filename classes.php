12b0d8f4ff2a870f83750655ef50d0636104130c
Raymon den Hartogh <raydenhartogh@gmail.com>
2018-03-18 20:40:17 +0100

    zondag avond

--- classes.php
+++ classes.php
@@ -1,35 +1,66 @@
 <?php
 
 class Ship {
-    public $name;
-    public $boatLength;
-    public $cord;
-   
-    
-    
-    public function __construct($theName, $theBoatLength, $cord){
-            $this->name = $theName;
-            $this->boatLength = $theBoatLength;
-            $this->cord = $cord;
+  public $name;
+  public $boatLength;
+  public $cord;
+
+ 
+ 
+  public function __construct($theName, $theBoatLength, $cord){
+          $this->name = $theName;
+          $this->boatLength = $theBoatLength;
+          $this->cord = $cord;
+       
           
-    }
-        
+  }
+     
 }
 
-  $destroyer = new Ship('Destroyer', 2, ["00","01"]);
 
-  $submarine = new Ship('Submarine', 3, ["26","36","46"]);
-  
-  $cruiser = new Ship('Cruiser', 3, ["71", "81", "91"]);
-  
-  $battleship = new Ship('Battleship', 4, ["96","97","98","99"]);
-  
-  $carrier = new Ship('Carrier', 5, ["08","18","28","38","48"]);
- 
-  $allShips = array_merge($destroyer->cord, $submarine->cord, $cruiser->cord, $battleship->cord, $carrier->cord);
-  
-    //$ = ["00", "01", "02"];
-//var_dump($boot1);
-  
-  
- 
+//$codestroyer = explode(",", $_GET['codestroyer']);
+//$cosubmarine = explode(",", $_GET['cosubmarine']);
+//$cocruiser = explode(",", $_GET['cocruiser']);
+//$cobattleship = explode(",", $_GET['cobattleship']);
+//$cocarrier = explode(",", $_GET['cocarrier']);
+
+$filterdestroyer = filter_input(INPUT_GET, 'codestroyer');
+$filtersubmarine = filter_input(INPUT_GET, 'cosubmarine');
+$filtercruiser = filter_input(INPUT_GET, 'cocruiser');
+$filterbattleship = filter_input(INPUT_GET, 'cobattleship');
+$filtercarrier = filter_input(INPUT_GET, 'cocarrier');
+
+$codestroyer = explode(",", $filterdestroyer);
+$cosubmarine = explode(",", $filtersubmarine);
+$cocruiser = explode(",", $filtercruiser);
+$cobattleship = explode(",", $filterbattleship);
+$cocarrier = explode(",", $filtercarrier);
+
+//echo $codestroyer[0];
+//echo $codestroyer[1];
+
+//echo var_dump($filterdestroyer);
+//echo var_dump($codestroyer);
+echo "<br>";
+
+//$var = ["00"];
+
+$destroyer = new Ship('Destroyer', 2, $codestroyer);
+//echo "<br><br>" . var_dump($destroyer);
+
+$submarine = new Ship('Submarine', 3, $cosubmarine);
+//echo "<br><br>" . var_dump($submarine);
+
+$cruiser = new Ship('Cruiser', 3, $cocruiser);
+
+$battleship = new Ship('Battleship', 4, $cobattleship);
+
+$carrier = new Ship('Carrier', 5, $cocarrier);
+
+$allShips = array_merge($destroyer->cord, $submarine->cord, $cruiser->cord, $battleship->cord, $carrier->cord);
+
+//echo var_dump($allShips);
+
+
+
+?>
